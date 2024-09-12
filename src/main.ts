fa0d379773f3946ac73e03d7a831cb7d2df1c27b
wuaidedidi <470357714@qq.com>
2024-09-12 12:41:34 +0800

    build: 构建项目配置，dev和build配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router/index";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import '@/assets/styles/global.scss'
+import { createPinia } from 'pinia' // 引入 Pinia
+const app = createApp(App)
 
-createApp(App).use(router).mount("#app");
+app.use(router)
+app.use(ElementPlus)
+const pinia = createPinia() // 创建 Pinia 实例
+app.use(pinia) // 使用 Pinia
+app.mount('#app')
