9dea38201e7fe47511e6ea34bc570d15d1cc194a
wuaidedidi <470357714@qq.com>
2024-10-14 19:28:40 +0800

    chore: 更新构建配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,23 @@ import electron from 'vite-plugin-electron/simple'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import commonjs from '@rollup/plugin-commonjs'
+// import commonjs from '@rollup/plugin-commonjs'
 
 export default defineConfig({
+  base: './',
   server: {
     host: 'localhost',
     port: 3000,
   },
   plugins: [
     vue(),
-    commonjs({
-      dynamicRequireTargets: [
-        path.resolve(__dirname, 'build/node_sqlite3.node'), // 确保路径正确
-      ],
-      ignoreDynamicRequires: false, // 可以尝试设置为 false
-    }),
+    // commonjs({
+    //   dynamicRequireTargets: [
+    //     path.resolve(__dirname, 'build/node_sqlite3.node'), // 确保路径正确
+    //   ],
+    //   ignoreDynamicRequires: false, // 可以尝试设置为 false
+    //   include: /node_modules/, // 确保 node_modules 中的 commonjs 模块能够被正确处理
+    // }),
     electron({
       main: {
         entry: 'electron-main/index.ts',
@@ -34,6 +36,8 @@ export default defineConfig({
                 'sqlite3', // 确保 sqlite3 被外部化
                 'bcrypt',
                 'server/*',
+                'element-plus',
+                'dayjs',
               ],
             },
           },
@@ -63,13 +67,23 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      dayjs: path.resolve(__dirname, 'node_modules/dayjs'), // 使用正确路径
     },
   },
   optimizeDeps: {
-    include: ['sqlite3', 'axios'],
+    include: ['sqlite3', 'axios', 'element-plus', 'dayjs'],
   },
+  //控制的是前端的打包流程
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    minify: false, // 禁用压缩
+    sourcemap: true, // 启用 source map 方便调试
+    rollupOptions: {
+      plugins: [],
+      external: [
+        'dayjs/plugin/customParseFormat', // 将 dayjs 插件外部化
+      ],
+    },
   },
 })
